68eaa8d820f89420fc1b6375bd9d31391b43a940
YuePan <panyuemail@gmail.com>
2015-06-28 22:22:03 +0800

    add dialog

--- popup.js
+++ popup.js
@@ -54,65 +54,64 @@ function getCurrentTabUrl(callback) {
  * @param {function(string)} errorCallback - Called when the image is not found.
  *   The callback gets a string that describes the failure reason.
  */
-function getImageUrl(searchTerm, callback, errorCallback) {
+function getMessage(url, query, callback, errorCallback) {
   // Google image search - 100 searches per day.
-  // https://developers.google.com/image-search/
-  var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images' +
-    '?v=1.0&q=' + encodeURIComponent(searchTerm);
+  var searchUrl = 'http://localhost:8080/search' +
+    '?v=1.0&q=' + encodeURIComponent(query) + '&u=' + url;
   var x = new XMLHttpRequest();
   x.open('GET', searchUrl);
-  // The Google image search API responds with JSON, so let Chrome parse it.
   x.responseType = 'json';
   x.onload = function() {
-    // Parse and process the response from Google Image Search.
+    // Parse and process the response from Auriga server.
     var response = x.response;
-    if (!response || !response.responseData || !response.responseData.results ||
-        response.responseData.results.length === 0) {
-      errorCallback('No response from Google Image search!');
+    if (!response || !response.data) {
+      errorCallback('No response from Auriga!');
       return;
     }
-    var firstResult = response.responseData.results[0];
-    // Take the thumbnail instead of the full image to get an approximately
-    // consistent image size.
-    var imageUrl = firstResult.tbUrl;
-    var width = parseInt(firstResult.tbWidth);
-    var height = parseInt(firstResult.tbHeight);
-    console.assert(
-        typeof imageUrl == 'string' && !isNaN(width) && !isNaN(height),
-        'Unexpected respose from the Google Image Search API!');
-    callback(imageUrl, width, height);
+    var message = response.data;
+    callback(message);
   };
   x.onerror = function() {
     errorCallback('Network error.');
   };
+  console.log("sending");
   x.send();
 }
 
-function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
+function renderMessage(messageText) {
+  document.getElementById('message').textContent = messageText;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   getCurrentTabUrl(function(url) {
     // Put the image URL in Google search.
-    renderStatus('Performing Google Image search for ' + url);
+    console.log('Check task related message for ' + url);
+    renderMessage('...');
 
-    getImageUrl(url, function(imageUrl, width, height) {
-
-      renderStatus('Search term: ' + url + '\n' +
-          'Google image search result: ' + imageUrl);
-      var imageResult = document.getElementById('image-result');
-      // Explicitly set the width/height to minimize the number of reflows. For
-      // a single image, this does not matter, but if you're going to embed
-      // multiple external images in your page, then the absence of width/height
-      // attributes causes the popup to resize multiple times.
-      imageResult.width = width;
-      imageResult.height = height;
-      imageResult.src = imageUrl;
-      imageResult.hidden = false;
-
-    }, function(errorMessage) {
-      renderStatus('Cannot display image. ' + errorMessage);
-    });
+    getMessage(url, "",
+      function(message) {
+        console.log('Auriga: ' + message);
+        renderMessage(message);
+      }, 
+      function(errorMessage) {
+        renderMessage('Cannot retrieve task related message. ' + errorMessage);
+      });
   });
+});
+
+document.addEventListener("keypress", function(event) {
+  if (event.keyCode == 13) {
+    getCurrentTabUrl(function(url) {
+      query = document.getElementById('command').value;
+      getMessage(url, query,
+        function(message) {
+          console.log('Auriga: ' + message);
+          renderMessage(message);
+        }, 
+        function(errorMessage) {
+          renderMessage('Cannot retrieve task related message. ' + errorMessage);
+        });
+        document.getElementById('command').value = "";
+    });
+  };
 });
